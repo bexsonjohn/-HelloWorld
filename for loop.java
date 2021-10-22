243dd8aaa1e8037ce7311b467918118a7874d85f
barbarian-vijin <92709692+barbarian-vijin@users.noreply.github.com>
2021-10-22 10:50:03 +0530

    Add files via upload

---
+++ for loop.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+ class forloops {
+
+	public static void main(String[] args) {
+		Scanner number = new Scanner(System.in);
+		int  numbers=number.nextInt();
+		System.out.println("Enter a number");
+		for (int i=0;i<=numbers;i++) {
+			System.out.println(i);
+		}
+		
+
+		
+
+	}
+
+}
